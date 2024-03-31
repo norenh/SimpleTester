406244d16c03452af593fb0b0f59c126e2f11686
norenh <henning.noren@gmail.com>
2024-03-31 19:44:40 +0200

    Change scripts to use latest selenium (4.19.1)

--- getdep.sh
+++ getdep.sh
@@ -1,4 +1,4 @@
-wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.18.0/selenium-java-4.18.1.zip
-unzip selenium-java-4.18.1.zip -d selenium-java-4.18.1
-chmod u+w selenium-java-4.18.1/*-sources.jar
-rm selenium-java-4.18.1/*-sources.jar
+wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.19.0/selenium-java-4.19.1.zip
+unzip selenium-java-4.19.1.zip -d selenium-java-4.19.1
+chmod u+w selenium-java-4.19.1/*-sources.jar
+rm selenium-java-4.19.1/*-sources.jar
